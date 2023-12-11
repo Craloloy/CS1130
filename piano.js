1a06cbfd731826ffb13790d04d92306c41ab7a95
Carl Roque <143686640+Craloloy@users.noreply.github.com>
2023-12-11 07:53:27 +0000

    first commit

---
+++ piano.js
@@ -0,0 +1,42 @@
+// Key names
+const keyNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+// Notes to be played for Baa, Baa, Black Sheep
+const blackSheepNotes = ['E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'D4', 'D4', 'D4', 'E4', 'G4', 'G4', 'E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'E4', 'D4', 'D4', 'E4', 'D4', 'C4'];
+
+// Delays for each note (in milliseconds)
+const noteDelays = [0, 500, 1000, 1500, 2000, 2500, 3000, 4000, 4500, 5000, 6000, 6500, 7000, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000];
+
+// Function to play a note
+function playNoteWithDelay(note, delay) {
+  setTimeout(() => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(note, '8n');
+  }, delay);
+}
+
+// Function to render the keys and their functionality
+function renderKeys() {
+  const piano = document.querySelector('.piano');
+
+  keyNames.forEach((key, index) => {
+    const keyDiv = document.createElement('div');
+    keyDiv.className = 'key';
+    keyDiv.dataset.note = blackSheepNotes[index];
+    keyDiv.textContent = key;
+    keyDiv.addEventListener('click', () => playNoteWithDelay(blackSheepNotes[index], 0));
+    piano.appendChild(keyDiv);
+  });
+}
+
+// Function to play all notes in the 'Baa, Baa, Black Sheep' song with delays
+function playBlackSheepWithDelays() {
+  blackSheepNotes.forEach((note, index) => {
+    playNoteWithDelay(note, noteDelays[index]);
+  });
+}
+
+// Render the keys
+renderKeys();
+
+
